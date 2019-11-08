63bd5fff3cca4496463640f9013540b0da50790a
Serkan Kumru <serkankumru1997@gmail.com>
2019-11-08 09:16:07 +0300

    Update ETicaretDB.cs

--- ETicaretDB.cs
+++ ETicaretDB.cs
@@ -34,6 +34,6 @@ namespace DAL
         // Add a DbSet for each entity type that you want to include in your model. For more information 
         // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
 
-         public virtual DbSet<Category> Category { get; set; }
+        
     }
 }
